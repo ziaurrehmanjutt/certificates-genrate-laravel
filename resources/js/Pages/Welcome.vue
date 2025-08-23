617802ea62462d52d64d80409b66ca2663e7750b
Zia <ziaa520@gmail.com>
2025-08-23 15:01:55 +0500

    update

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -64,12 +64,11 @@ function handleImageError() {
                         </Link>
 
                         <template v-else>
-                            <Link
-                                :href="route('login')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                            >
-                                Log in
-                            </Link>
+
+                          <a href="/admin/login" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                Login
+                          </a>
+                          
 
                             <Link
                                 v-if="canRegister"
